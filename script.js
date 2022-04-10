562069daa1cd37d132255906094514b9c7e9b6e5
ciparkin1 <100965233+ciparkin1@users.noreply.github.com>
2022-04-10 11:14:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+$("btn-info").on('submit', handleFormSubmit);
+
+$(function () {
+    $('#btn btn-info').submit();
+  });
+
+  $("#displayform").hide(); 
+
+  addEventListener(Click.btn btn-info)
+  $('#projectModal').dialogue;
+  $('#projectModal').modal('hide');
